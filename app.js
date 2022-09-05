b9690d9d7ca454ef57d8e41c53531f4fe689dea2
Stallone TCHOUDI <stallonetchoudi@yahoo.fr>
2022-09-05 14:59:14 +0200

    app.js: Engine coffee

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'coffee');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyPaser.urlencoded({ extended: true}));
